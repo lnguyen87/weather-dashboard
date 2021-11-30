fad70e917d6dc68f6ba32685f77378c2169b4b84
lnguyen87 <luc_nguyen@outlook.com>
2021-11-30 11:36:12 -0700

    created for loop to cycle through api array

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,50 +1,43 @@
-var cityName = $(".cityName");
+var cityName = $(".cityName").val();
 var date = $(".date");
 var icon = $(".icon");
 var temperature = $(".temperature");
 var wind = $(".wind");
 var humidity = $(".humidity");
 
+function displayWeather(d) {
+    var celsius = Math.round((parseFloat(d.list[0].main.temp)-32)*.5556);
 
-// $(".searchButton").on("click", function() {
-//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=london&appid=1750d64b9b244e082f61b1c95f2ee8c2';
-//     fetch(apiUrl)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-//             var dateValue = data['name'];
-//             var iconValue = data['weather'][0]['description'];
-//             var temperatureValue = data['main']['temp'];
-//             var windValue = data['wind']['speed'];
-//             var humidityValue = data['main']['humidity'];
+    document.querySelector(".date0").innerHTML = d.city.name + " " + d.list[0].dt_txt;
+    document.querySelector(".wind0").innerHTML = "Wind Speed: " +  d.list[0].wind.speed;
+    document.querySelector(".temperature0").innerHTML = "Temperature: " +  d.list[0].main.temp + '&deg;' + " Fahrenheit " + " / " + celsius + '&deg;' + " Celsius";
+    document.querySelector(".humidity0").innerHTML = "Humidity: " + d.list[0].main.humidity + "%";
+    document.querySelector(".uvIndex").innerHTML = "UV Index: " + d.list[0]
+}
 
-//             date.innerHTML = dateValue;
-//             icon.innerHTML = iconValue;
-//             temperature.innerHTML = temperatureValue;
-//             wind.innerHTML = windValue;
-//             humidity.innerHTML = humidityValue;
 
-//     })
-//     .catch(error => alert("City not found! Please enter a new city."))
-
-// });
-function displayWeather( d ) {
-    var celsius = Math.round((parseFloat(d.main.temp)-32)*.5556);
-
-    document.querySelector(".wind").innerHTML = "Wind Speed: " +  d.wind.speed;
-    document.querySelector(".temperature").innerHTML = "Temperature: " +  d.main.temp + '&deg;' + " Fahrenheit " + " / " + celsius + '&deg;' + " Celsius";
-    document.querySelector(".humidity").innerHTML = "Humidity: " + d.main.humidity + "%";
+function displayForecast( d ) {
+    for (var i = 1; i < 6; i++) {
+                        
+    var celsius = Math.round((parseFloat(d.list[i].main.temp)-32)*.5556);
+    
+    document.querySelector(".date1").innerHTML = d.list[i].dt_txt;
+    document.querySelector(".wind1").innerHTML = "Wind Speed: " +  d.list[i].wind.speed;
+    document.querySelector(".temperature1").innerHTML = "Temperature: " +  d.list[i].main.temp + '&deg;' + " Fahrenheit " + " / " + celsius + '&deg;' + " Celsius";
+    document.querySelector(".humidity1").innerHTML = "Humidity: " + d.list[i].main.humidity + "%";
+    }
 }
 
 $(".searchButton").on("click", function() {
 var getCoordinates = function(cityName) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=1750d64b9b244e082f61b1c95f2ee8c2&units=imperial';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&cnt=6&appid=1750d64b9b244e082f61b1c95f2ee8c2&units=imperial';
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
                     displayWeather(data);
+                    displayForecast(data);
                 })
             } else {
                 alert("Location not found");
