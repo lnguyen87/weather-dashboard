9fa5186d81da11e5a814c9c74a3b23af24a9d02d
lnguyen87 <luc_nguyen@outlook.com>
2021-11-29 23:49:51 -0700

    created containers to display weather info that is pull from openweather api

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,32 +1,58 @@
-console.log("this is a test");
+var cityName = $(".cityName");
+var date = $(".date");
+var icon = $(".icon");
+var temperature = $(".temperature");
+var wind = $(".wind");
+var humidity = $(".humidity");
 
-var getCoordinates = function(locationName) {
-    var apiUrl = "http://api.positionstack.com/v1/forward?access_key=87440e36f16f0d6657eecd88d154cc1d&query=" + locationName + "&limit=1&output=json";
+
+// $(".searchButton").on("click", function() {
+//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=london&appid=1750d64b9b244e082f61b1c95f2ee8c2';
+//     fetch(apiUrl)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+//             var dateValue = data['name'];
+//             var iconValue = data['weather'][0]['description'];
+//             var temperatureValue = data['main']['temp'];
+//             var windValue = data['wind']['speed'];
+//             var humidityValue = data['main']['humidity'];
+
+//             date.innerHTML = dateValue;
+//             icon.innerHTML = iconValue;
+//             temperature.innerHTML = temperatureValue;
+//             wind.innerHTML = windValue;
+//             humidity.innerHTML = humidityValue;
+
+//     })
+//     .catch(error => alert("City not found! Please enter a new city."))
+
+// });
+function displayWeather( d ) {
+    var celsius = Math.round((parseFloat(d.main.temp)-32)*.5556);
+
+    document.querySelector(".wind").innerHTML = "Wind Speed: " +  d.wind.speed;
+    document.querySelector(".temperature").innerHTML = "Temperature: " +  d.main.temp + '&deg;' + " Fahrenheit " + " / " + celsius + '&deg;' + " Celsius";
+    document.querySelector(".humidity").innerHTML = "Humidity: " + d.main.humidity + "%";
+}
+
+$(".searchButton").on("click", function() {
+var getCoordinates = function(cityName) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=1750d64b9b244e082f61b1c95f2ee8c2&units=imperial';
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
+                    displayWeather(data);
                 })
             } else {
                 alert("Location not found");
             }
         })
         .catch(function(error) {
-            alert("unable to connect to position stack");
+            alert("City not found! Please enter a new city.");
         });
-}
-
-getCoordinates("Salt Lake City, UT");
-
-$(".searchBtn").on("click", function() {
-    var locationName = formData;
-    var locationData = getCoordinates(locationName)
-    var newLocation = {
-        city: locationData.name,
-        lat: locationData.latitude,
-        long: locationData.longitude,
     }
-    locationArr.push(newLocation);
-    var newLocationButton = $("<button>").addClass("btn").attr("id", newLocation.city);
+    getCoordinates("Salt Lake City");
 })
